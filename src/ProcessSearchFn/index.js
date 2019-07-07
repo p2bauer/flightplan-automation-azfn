3779c6dcead9367b00617c7d8ad81d1f8179f527
Paul Bauer <paulb@moj.io>
2019-07-06 20:54:42 -0400

    cleanup

--- src/ProcessSearchFn/index.js
+++ src/ProcessSearchFn/index.js
@@ -3,17 +3,6 @@ const cliSearch = require('../flightplan/shared/search');
 module.exports = async function(context, mySbMsg) {
     context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg);
 
-    // try {
-    //     // Open database
-    //     console.log('Opening database...');
-    //     await db.open();
-    //     console.log('Success!');
-    //   } catch (err) {
-    //     console.error(err);
-    // }
-
-    //headless, proxy, docker 
-
     mySbMsg.headless = true;
     mySbMsg.proxy = "";
     mySbMsg.docker = false;
@@ -22,9 +11,5 @@ module.exports = async function(context, mySbMsg) {
 
     await cliSearch.doSearch(mySbMsg, credentialsOverride);
 
-
-
     // TODO: do a nodejs HTTP Post after search is complete to post to the PushBullet API
-
-    
 };
