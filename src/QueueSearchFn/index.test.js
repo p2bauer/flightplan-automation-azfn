d989ad5a11b30491e5ac14350e21b2292a09b0d6
Paul Bauer <paulb@moj.io>
2019-07-09 20:41:04 -0400

    tests are passing....for now...

--- src/QueueSearchFn/index.test.js
+++ src/QueueSearchFn/index.test.js
@@ -4,11 +4,20 @@ const context = require('../testing/defaultContext');
 test('Http trigger should return known text', async () => {
 
     const request = {
-        query: { fromCity: 'nrt', toCity: 'hnl' }
+        query: { 
+            fromCity: 'nrt', 
+            toCity: 'hnl', 
+            start: '2020-01-01', 
+            end: '2020-01-02', 
+            quantity: 2
+        }
     };
 
     await queueSearchFunction(context, request);
 
-    expect(context.log.mock.calls.length).toBe(1);
-    expect(context.res.body).toEqual('Searching for nrt to hnl');
+    // TODO: don't test logs, test something more useful!
+    expect(context.log.mock.calls.length).toBe(2);
+
+    expect(context.res.body).toEqual('Searching for ' + request.query.fromCity + ' to ' + request.query.toCity + 
+        ' from ' + request.query.start + ' to ' + request.query.end + ' for ' + request.query.quantity + ' person(s)');
 });
